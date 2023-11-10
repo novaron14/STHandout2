ec0c3af79e9edb14d42eb10be6ea218c9a1760c4
novaron14 <novaron14@gmail.com>
2023-11-10 23:01:28 +0100

    second iteration

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ export function greet(names: string[]): string {
   if (names.length === 0) {
     return 'Hello, my friend';
   }
-
-  return '';
+  const normalNames = names.filter(str => /[a-z]/.test(str));
+  let normalGreetings: string = '';
+  if(normalNames.length==1){
+    normalGreetings = `Hello ${normalNames[0]}!`
+  }
+  return normalGreetings;
 }
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -7,16 +7,3 @@ test('handles no input', () => {
 test('handles single input', () => {
   expect(greet(['Bob'])).toBe('Hello Bob!');
 });
-
-test('handles multiple inputs', () => {
-  expect(greet(['Alice', 'Bob', 'Jerry'])).toBe('Hello Alice, Bob and Jerry!');
-});
-
-test('handles shouting input', () => {
-  expect(greet(['BARRY'])).toBe('HELLO BARRY!');
-  expect(greet(['BARRY', 'KATE'])).toBe('HELLO BARRY AND KATE!');
-});
-
-test('handles mixed input', () => {
-  expect(greet(['JAY', 'Maya', 'Alice', 'BOB', 'Charlotte'])).toBe('Hello Maya, Alice and Charlotte. HELLO JAY AND BOB!');
-});
