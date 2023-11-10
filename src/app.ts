145f19855876c63fce5e13b5e35bcfd2592a13f2
novaron14 <novaron14@gmail.com>
2023-11-10 23:08:58 +0100

    fourth iteration

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,16 @@ export function greet(names: string[]): string {
     return 'Hello, my friend';
   }
   const normalNames = names.filter(str => /[a-z]/.test(str));
+  const upperCaseNames = names.filter(str => /^[A-Z]+$/.test(str));
   let normalGreetings: string = '';
+  let shoutingGreetings: string = '';
   if(normalNames.length==1){
     normalGreetings = `Hello ${normalNames[0]}!`
   }else if(normalNames.length>1){
     normalGreetings = `Hello ${normalNames.slice(0, -1).join(', ')} and ${normalNames.slice(-1)}!`;
   }
-  return normalGreetings;
+  if(upperCaseNames.length==1){
+    shoutingGreetings = `HELLO ${upperCaseNames[0]}!`
+  }
+  return `${shoutingGreetings} ${normalGreetings}`.trim();
 }
--- tests/app.test.ts
+++ tests/app.test.ts
@@ -16,3 +16,7 @@ test('handles multiple inputs', () => {
 test('handles multiple inputs', () => {
   expect(greet(['Alice', 'Bob', 'Jerry'])).toBe('Hello Alice, Bob and Jerry!');
 });
+
+test('handles single uppercase input', () => {
+  expect(greet(['BOB'])).toBe('HELLO BOB!');
+});
